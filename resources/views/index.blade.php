8c0ef08ce0bb7cf5bbc9f51fc1db9f376d277f44
Klemens Litano <klemenslr12@gmail.com>
2019-10-19 11:41:02 +0700

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+<?php
+use PEAR2\Net\RouterOS;
+
+header('Content-Type: text/plain');
+
+try {
+	$client = new RouterOS\Client('10.21.0.152', 'admin', 'mikman123!');
+    	$util = new RouterOS\Util($client);
+    	foreach ($util->setMenu('/log')->getAll() as $entry) {
+        	echo $entry('time') . ' ' . $entry('topics') . ' ' . $entry('message') . '<br>';
+    	}
+    	echo $printRequest;
+} catch (Exception $e) {
+    echo 'Unable to connect to RouterOS.';
+}
---
+++ resources/views/ipall.blade.php
@@ -0,0 +1,23 @@
+<?php
+use PEAR2\Net\RouterOS;
+
+#include 'PEAR2_Net_RouterOS-1.0.0b6';
+require_once 'PEAR2_Net_RouterOS-1.0.0b6/src/PEAR2/Autoload.php';
+
+header('Content-Type: text/plain');
+
+try {
+   $client = new RouterOS\Client('10.21.0.152', 'admin', 'mikman123!');
+	// Ambil List IP
+	$getIPs = $client->sendSync(new RouterOS\Request('/ip address print'));
+	print_r($getIPs);
+	// foreach ($getIPs as $getIP){
+	//     if($getIP->getType() === RouterOS\Response::TYPE_DATA) {
+ //      		echo 'IP: ', $getIP->getProperty('address'),
+	//       ' Interface: ', $getIP->getProperty('interface'),
+	//       "\n";
+	//     }
+	// }
+} catch (Exception $e) {
+  die ($e);
+}
