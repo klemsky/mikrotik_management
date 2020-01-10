4b5f456e2b2dc329b782fd36da7bb151421a572c
Klemens Litano <klemenslr12@gmail.com>
2020-01-10 18:53:45 +0700

    Generate Link Enhancement, Way cooler than before :p

--- public/js/mikman.js
+++ public/js/mikman.js
@@ -1,5 +1,4 @@
 $(document).ready(function() { 
-    $('.toggle').binus_toggle();
     $countAccessList = 1;
 
     $("#add-ip").on("click", function() { 
@@ -40,6 +39,17 @@ $(document).ready(function() {
         e.preventDefault();
         insertRegistrationForm();
         $('#btn-submit-form').prop('disabled',true);
+    });
+
+    $('#btnCopyTC').hide();
+    $('#ticket_submitBtn').click(function(e){
+        e.preventDefault();
+        showLink();
+        $('#ticket_submitBtn').prop('disabled',true);
+    });
+    $('#btnCopyTC').click(function(e){
+        copyTC();
+        $('#ticket_submitBtn').prop('disabled',false);
     });
 });
 
@@ -483,13 +493,27 @@ function showLink(){
                 //     window.location.href = "/ip";
                 // },700);
                 $("#link").val(response.link);
+                $('#btnCopyTC').show();
                 // console.log('')
             }else{
                 // $('.is-error').show();
                 // $('.is-success').hide();
                 // $('.is-error').html(data);
                 showError(response.status);
+                $('#ticket_submitBtn').prop('disabled',false);
             }
         },
     });
+}
+
+function copyTC(){
+    var copyText = $('#link');
+
+    copyText.select();
+    document.execCommand("copy");
+    showSuccess("Link copied to clipboard");
+}
+
+function changeAsIType(){
+    $('#link').val('');
 }
